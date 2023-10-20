f13a1ed04348a3ed90432fb7aa7b1fd6e78df22d
Preetam Raj <iitianpreetam@gmail.com>
2023-10-20 21:23:38 +0530

    removed vercel deploy plugin for sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,6 @@
 
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
-import { vercelDeployTool } from 'sanity-plugin-vercel-deploy';
 import { deskTool } from 'sanity/desk';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
@@ -27,7 +26,6 @@ export default defineConfig({
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
-    vercelDeployTool(),
   ],
   theme: sanityTheme,
   studio: {
