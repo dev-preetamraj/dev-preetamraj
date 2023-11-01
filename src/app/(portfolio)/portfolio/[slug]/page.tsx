96e0d8762c2e9d6f255eb8a8094e4fc9cc372a9f
Preetam Raj <iitianpreetam@gmail.com>
2023-11-01 14:13:19 +0530

    added revalidation

--- src/app/(portfolio)/portfolio/[slug]/page.tsx
+++ src/app/(portfolio)/portfolio/[slug]/page.tsx
@@ -15,7 +15,7 @@ type Props = {
   };
 };
 
-// export const revalidate = 60; // revalidate this page every 60 seconds
+export const revalidate = 60; // revalidate this page every 60 seconds
 
 export async function generateStaticParams() {
   const query = groq`
