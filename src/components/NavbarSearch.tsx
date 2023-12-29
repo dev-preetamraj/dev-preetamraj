11a2d6aa128446f067dc3cf92c5f52dee0cbdb17
Preetam Raj <iitianpreetam@gmail.com>
2023-12-29 21:14:46 +0530

    refactor: added interactive searchbar in header

--- src/components/NavbarSearch.tsx
+++ src/components/NavbarSearch.tsx
@@ -1,33 +1,96 @@
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
-import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline';
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
-import { Button } from './ui/button';
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { DesktopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { SearchIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 const NavbarSearch = () => {
+  const { setTheme } = useTheme();
+  const [open, setOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  const isDesktop = useMediaQuery('(min-width: 768px)');
+
+  useEffect(() => setMounted(true), []);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant='outline' className='flex items-center space-x-20'>
-            <span>Search</span>
-            <div className='flex items-center space-x-2'>
-              <AdjustmentsHorizontalIcon className='h-4 w-4' />
-              <span>k</span>
-            </div>
-          </Button>
-        </DialogTrigger>
-        <DialogContent className='sm:max-w-[425px] p-0'>
-          <div className='flex items-center space-x-2 border-b border-primary p-4'>
-            <MagnifyingGlassIcon className='h-6 w-6' />
-            <input
-              type='text'
-              className='bg-transparent outline-none'
-              placeholder='Search...'
-            />
-          </div>
-          <div className='p-4 pt-0'>search results</div>
-        </DialogContent>
-      </Dialog>
+      <Button
+        variant='outline'
+        size={isDesktop ? 'default' : 'icon'}
+        onClick={() => setOpen(true)}
+        className='flex items-center md:space-x-4 xl:space-x-10 group'
+      >
+        <span className='text-muted-foreground group-hover:text-foreground hidden md:inline-block'>
+          Search project, blog or settings...
+        </span>
+        <div>
+          <SearchIcon className='icon block md:hidden' />
+          <kbd className='pointer-events-none hidden md:inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 text-muted-foreground group-hover:text-foreground'>
+            <span className='text-xs'>⌘</span>K
+          </kbd>
+        </div>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder='Type a command or search...' />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading='Theme'>
+            <CommandItem
+              onSelect={() => {
+                setTheme('dark');
+                setOpen(false);
+              }}
+            >
+              <MoonIcon className='mr-2 h-4 w-4' />
+              <span>Dark</span>
+            </CommandItem>
+
+            <CommandItem
+              onSelect={() => {
+                setTheme('light');
+                setOpen(false);
+              }}
+            >
+              <SunIcon className='mr-2 h-4 w-4' />
+              <span>Light</span>
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                setTheme('system');
+                setOpen(false);
+              }}
+            >
+              <DesktopIcon className='mr-2 h-4 w-4' />
+              <span>System</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+        </CommandList>
+      </CommandDialog>
     </>
   );
 };
