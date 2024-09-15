9b1f624b0e22f3ade95272831b64053c6f4f53f0
Preetam Raj <iitianpreetam@gmail.com>
2024-09-15 20:15:13 +0530

    feat: added preview image

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,6 +1,20 @@
 import { fetchBlogs } from '@/actions/blog';
 import BlogList from '@/components/blog/BlogList';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Preetam Raj',
+  description:
+    'A fullstack web/app developer passionate about creating robust application that can handle every user.',
+  openGraph: {
+    images: ['https://preetamraj.dev/preview.png'],
+  },
+  alternates: {
+    canonical: 'https://preetamraj.dev',
+  },
+};
+
 const HomePage = async () => {
   const blogs = await fetchBlogs();
 
