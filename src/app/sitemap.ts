c0131deae2c6155a3fcaae4b31ac1ac9c701c23b
Preetam Raj <iitianpreetam@gmail.com>
2024-09-15 19:33:42 +0530

    feat: updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,40 +3,70 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://preetamraj.vercel.app',
+      url: 'https://preetamraj.dev',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://preetamraj.vercel.app/about',
+      url: 'https://preetamraj.dev/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://preetamraj.vercel.app/portfolio',
+      url: 'https://preetamraj.dev/portfolio',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: 'https://preetamraj.vercel.app/blog',
+      url: 'https://preetamraj.dev/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: 'https://preetamraj.vercel.app/blog/prisma-with-postgres-in-nextjs',
+      url: 'https://preetamraj.dev/blog/prisma-with-postgres-in-nextjs',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.7,
     },
     {
-      url: 'https://preetamraj.vercel.app/blog/nextjs-starting-configurations',
+      url: 'https://preetamraj.dev/blog/nextjs-starting-configurations',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.7,
+    },
+    {
+      url: 'https://preetamraj.dev/portfolio/my-portfolio',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://preetamraj.dev/portfolio/mynote',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://preetamraj.dev/portfolio/baazarnow',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://preetamraj.dev/portfolio/hulu-clone',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://preetamraj.dev/portfolio/sms-school-management-system',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6,
     },
   ];
 }
