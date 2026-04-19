57a0d5643b39065032accef9779b6db0cefeb1b1
Preetam Raj <preetam.raj@neufin.co>
2026-04-19 23:08:19 +0530

    upgraded packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
-      <html lang='en'>
+      <html lang='en' suppressHydrationWarning>
         <body>
           <ThemeProvider
             attribute='class'
