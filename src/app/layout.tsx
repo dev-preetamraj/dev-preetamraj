529373f9efca092bcc1c1ed770011e69e51aece5
Preetam <iitianpreetam@gmail.com>
2026-07-10 23:40:11 +0530

    added /studio to robots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,15 @@ import { Toaster } from '@/components/ui/sonner';
 import Provider from '@/providers/Provider';
 import { ThemeProvider } from '@/providers/ThemeProvider';
 import '@/styles/globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://preetamraj.dev'),
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 export default function RootLayout({
   children,
